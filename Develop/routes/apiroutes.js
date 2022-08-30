a31e8059018e1c2c233c16f101b093c85422ab2a
kikigoestech <101837197+kikigoestech@users.noreply.github.com>
2022-08-30 14:41:19 -0400

    app.get functin

---
+++ Develop/routes/apiroutes.js
@@ -0,0 +1,25 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+const fs = require('fs');
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+  // Routes - one to index, one to notes
+  app.get('/api/notes', function(req, res) {
+    fs.readFile('./db/db.json', (err, data) => {
+      if (err) throw err;
+      dbData = JSON.parse(data);
+      res.send(dbData);
+    });
+  });
--- Develop/routes/apiroutes/notesRoutes.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const fs = require('fs');
-const path = require('path');
-const { v4: uuidv4 } = require('uuid');
-const db = require('../../db/db.json');
-
