143be477c4122db6c9fcfd4234cdcbeca34476b1
kikigoestech <101837197+kikigoestech@users.noreply.github.com>
2022-08-30 15:48:43 -0400

    Add note taker

--- server.js
+++ server.js
@@ -1,25 +1,37 @@
-// initiate express.js
-const express = require('express');
-const PORT = process.env.PORT || 3001;
-const app = express();
-const fs = require('fs');
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
 
-// instruct server to make files in 'public' dir readily available
+const express = require('express');
+
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
+const app = express();
+// Sets an initial port. We"ll use this later in our listener
+const PORT = process.env.PORT || 3001;
+
+// Sets up the Express app for data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static('public'));
 
-// parse incoming string & array data
-app.use(express.urlencoded({ extended: true }));
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+require('./routes/apiroutes')(app);
+require('./routes/htmlroutes')(app);
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
 
-// parse incoming JSON data
-app.use(express.json());
-
-// use router set up in 'routes' directory
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
-
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+app.listen(PORT, function() {
+  console.log(`Server is listening on PORT: ${PORT}`);
 });
-
